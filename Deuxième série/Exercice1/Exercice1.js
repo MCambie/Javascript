6b6baf921ffeb320e83c68ad25e1f4310f7dd943
MCambie <cambie.marie@gmail.com>
2018-05-17 16:25:07 +0200

    fin journee 17/05

---
+++ Deuxième série/Exercice1/Exercice1.js
@@ -0,0 +1,12 @@
+function surfaceRectangle() {
+      var largeur = Number(prompt("largeur"));
+      console.log(typeof largeur);
+      var longueur = Number(prompt("longueur"));
+      alert(largeur*longueur+"cm²");
+}
+function periRectangle() {
+    var largeur = Number(prompt("largeur"));
+    var longueur = Number(prompt("longueur"));
+    alert((largeur+longueur)*2 + "cm");
+
+}
