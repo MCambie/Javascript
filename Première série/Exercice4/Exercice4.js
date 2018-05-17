097c57e315f4fef337d444e977b1f81e8f9e4f05
MCambie <cambie.marie@gmail.com>
2018-05-17 14:34:43 +0200

    ex 17/05

---
+++ Première série/Exercice4/Exercice4.js
@@ -0,0 +1,7 @@
+function valider()
+        {
+            var nom = document.getElementById("nom").value;
+            var prenom = document.getElementById("prenom").value;
+            var ville = document.getElementById("ville").value;
+            alert("Prénom:" + prenom + "\n"+"Nom:"+ nom + "\n"+"Ville:" + ville);
+        }
