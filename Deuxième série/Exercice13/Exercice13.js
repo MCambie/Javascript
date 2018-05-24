535b6fb071a6090634cc16edd5982e8f511f5b41
MCambie <cambie.marie@gmail.com>
2018-05-24 16:23:14 +0200

    fin de journee 24/05

--- Deuxième série/Exercice13/Exercice13.js
+++ Deuxième série/Exercice13/Exercice13.js
@@ -1,48 +1,32 @@
-function conversionTemperature(){
-	var choix =  prompt("Faites un choix entre 0 et 6"+"\n"+"0 Fin du programme"+"\n"+
-"1 De Celsius vers Fahrenheit"+"\n"+"2 De Celsius vers Kelvin"+"\n"+"3 De Fahrenheit vers Celsius"+"\n"+
-"4 De Fahrenheit vers Kelvin"+"\n"+"5 De Kelvin vers Celsius"+"\n"+"6 De Kelvin vers Fahrenheit");
-	var temperature = prompt("entrez la température à convertir");
-	var tempK = temperature;
-	var tempC = temperature;
-	var tempF = temperature;
-	switch (choix) {
-
-		case "0":
-			alert("Fin du programme");
+function calculIMC(){
+	var poids = Number(prompt("Entrez votre poids"));
+	var taille = Number(prompt("Entrez votre taille"));
+	var calcul = poids/(taille*taille);
+	var imc = calcul.toFixed(2);
+	switch (true) {
+		case (imc < 16.5):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes en dénutrition ou famine");
 			break;
-
-		case "1":
-			tempF= (Number(tempC)* 9/5) + 32;
-			alert(tempF+"°F");
+		case (imc < 18.5):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes maigre");
 			break;
-
-		case "2":
-			tempK= Number(tempC)+ 273.15;
-			alert(tempK+"°K");
+		case (imc < 25):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes de corpulence normale");
 			break;
-
-		case "3":
-			tempC= (Number(tempF)- 32) / (9/5);
-			alert(tempC+"°C");
+		case (imc < 30):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes en surpoids");
 			break;
-
-		case "4":
-			tempK= ((Number(tempF) - 32) * (5/9)) + 273.15;
-			alert(tempK+"°K");
+		case (imc < 35):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes en obésité modéré");
 			break;
-
-		case "5":
-			tempC= Number(tempK) - 273,15;
-			alert(tempC+"°C");
+		case (imc < 40):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes en obésité sévère");
 			break;
-
-		case "6":
-			tempF= (Number(tempK) * 9/5) - 459.67;
-			alert(tempF+"°F");
+		case (imc > 40):
+			alert("Votre IMC est de "+imc+"\n"+ "Vous êtes en obésité morbide");
 			break;
-
 		default:
-				alert('Pardon,que voulez-vous?');
+			alert("Error");
+		}
+
 	}
-}
